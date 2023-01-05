a2868aea4fdea10ecbeea4ae47c34d26e7dff332
Richard Niemand <niemand.richard@gmail.com>
2023-01-05 10:42:42 -0700

    Fetching job enabled state from the DB

--- src/Rn.Timerr/Repos/JobsRepo.cs
+++ src/Rn.Timerr/Repos/JobsRepo.cs
@@ -11,15 +11,18 @@ interface IJobsRepo
 class JobsRepo : IJobsRepo
 {
   private readonly IConnectionFactory _connectionFactory;
+  public const string TableName = "Jobs";
 
   public JobsRepo(IConnectionFactory connectionFactory)
   {
     _connectionFactory = connectionFactory;
   }
 
+
+  // Interface methods
   public async Task<List<JobEntity>> GetJobsAsync()
   {
-    const string query = @"SELECT * FROM `Jobs` WHERE `Enabled` = 1";
+    const string query = @$"SELECT * FROM `{TableName}` WHERE `Enabled` = 1";
     await using var conn = _connectionFactory.GetConnection();
     return (await conn.QueryAsync<JobEntity>(query)).ToList();
   }
